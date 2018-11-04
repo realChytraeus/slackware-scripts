8eeaf9a4ffb0926dd8500f86a6965356e780489d
realChytraeus <chytraeu@sdf.org>
2018-11-04 15:48:15 -0600

    adding a bash script for mostly vanilla kernel
    This is a bash script to automate building the stable series of 4.4.x kernel on Slackware. It should be trivial to adapt its use for the other series of kernels as well. It downloads the Linux kernel source, applies a GCC optimization patch, compiles it, and then copies the files to /boot.

---
+++ slackware-vanilla-kernel.sh
@@ -0,0 +1,87 @@
+#!/bin/bash
+# Greetings.
+# A rather vanilla kernel with gcc optimization patch
+# Donald Cooley <chytraeu@sdf.org>
+# INSERT LICENSE HERE.
+VERSION=$1
+if [ -z $VERSION ]; then
+	echo "kernel version?"
+	exit 0
+fi
+
+jobs='9'
+KERNEL_FILES="/usr/src/linux/arch/x86/boot"
+KERNEL_SERVER="https://www.kernel.org/pub/linux/kernel/v4.x/linux"
+SOURCE_DIRECTORY='/usr/src'
+
+#set -e
+
+# Donwload the kernel source and verify its gpg signature
+
+fetch_kernel () {
+    echo 'Downloading kernel source code and verifying its gpg signature'
+    cd $SOURCE_DIRECTORY
+    aria2c -x5 $KERNEL_SERVER-$VERSION.tar.xz
+    wget $KERNEL_SERVER-$VERSION.tar.sign
+    xz -dc linux-$VERSION.tar.xz | gpg --verify linux-$VERSION.tar.sign -
+    return
+}
+
+# Decompress kernel and link against our soon-to-be patched kernel
+
+decompress_kernel () {
+    echo "Decompressing kernel"
+    cd $SOURCE_DIRECTORY
+    tar -xf linux-$VERSION.tar.xz -C $SOURCE_DIRECTORY
+    rm linux
+    ln -s linux-$VERSION linux
+    return
+}
+
+# Patch kernel with a GCC optimization patch found here:
+# https://github.com/graysky2/kernel_gcc_patch
+patch_kernel () {
+    echo "Applying a GCC optimization patch and our present working kernel's configuration"
+    cd $SOURCE_DIRECTORY/linux
+    make mrproper
+    patch -p1 < ../enable_additional_cpu_optimizations_for_gcc_v4.9+_kernel_v3.15+.patch
+    zcat /proc/config.gz > .config
+    make oldconfig 
+    echo "Don't forget to run /usr/share/mkinitrd/mkinitrd_command_generator.sh \
+        and to edit /etc/lilo.conf. Finally, run lilo again."
+    return
+}
+
+# Compile kernel
+compile_kernel () {
+    echo "Compiling kernel. This will take some time ..."
+    time ( make -j$jobs bzImage modules && make modules_install )
+    return
+}
+
+# Move files into place
+move_kernel () {
+    echo "Moving kernel and its files into /boot"
+    cp System.map /boot/System.map-$VERSION-custom
+    cp $KERNEL_FILES/bzImage /boot/vmlinuz-$VERSION-custom
+    cd /boot
+    rm System.map
+    ln -s System.map-$VERSION-custom System.map
+    return
+}
+
+
+cd $SOURCE_DIRECTORY
+# Donwload kernel's gpg key
+echo "Retrieve gpg key for our kernel source code"
+gpg --recv-keys 6092693E
+
+fetch_kernel
+
+decompress_kernel
+
+patch_kernel
+
+compile_kernel
+
+move_kernel
